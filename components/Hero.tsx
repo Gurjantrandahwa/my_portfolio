e1ccd431b2ebe6a2200c660b0e8bf0de1a5138f7
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2022-10-11 17:49:16 +0530

    my portfolio

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,9 +26,9 @@ export default function Hero() {
                 <Link href={"#about"}>
                     <button className={"heroButton"}>About</button>
                 </Link>
-                <Link href={"#experience"}>
-                    <button className={"heroButton"}>Experience</button>
-                </Link>
+                {/*<Link href={"#experience"}>*/}
+                {/*    <button className={"heroButton"}>Experience</button>*/}
+                {/*</Link>*/}
                 <Link href={"#skills"}>
                     <button className={"heroButton"}>Skills</button>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Header from "../components/Header";
 import Hero from '../components/Hero';
 import About from "../components/About";
-import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import ContactMe from "../components/ContactMe";
@@ -24,12 +23,6 @@ const Home: NextPage = () => {
         <section id={"about"} className={"snap-center"}>
             <About/>
         </section>
-
-
-        <section id={"experience"} className={"snap-center"}>
-            <WorkExperience/>
-        </section>
-
 
         <section id={"skills"} className={"snap-start"}>
             <Skills/>
